c89d45dd988b24eba4d028db0c3d270b6f6669dd
Ali <montazerion.dev@gmail.com>
2025-03-30 06:17:35 +0330

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ import './globals.css';
 export default function Home() {
   return (
     <div className="w-full bg-black h-screen flex justify-center items-center font-bold text-lg ">
-      <div className="border border-white p-8 flex text-white">hello ali</div>
+      <div className="border border-white p-8 flex text-white">
+        Enghelab University
+      </div>
     </div>
   );
 }
